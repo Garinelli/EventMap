6a683170ba6ec5dd6e38826debc1e2ab97a5beb4
Yuri <yura.voskanyan.2003@mail.ru>
2025-02-27 21:52:52 +0700

    refactor position.

--- app/src/main/java/com/example/eventmap/Registration.java
+++ app/src/main/java/com/example/eventmap/Registration.java
@@ -1,4 +1,13 @@
 package com.example.eventmap;
 
-public class Registration {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Registration extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
 }
