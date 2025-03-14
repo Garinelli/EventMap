4fd997650b68e7b383e73218e042366e233b7e60
Yuri <yura.voskanyan.2003@mail.ru>
2025-03-14 14:02:42 +0700

    Correct display of the registration page.

--- app/src/main/java/com/example/eventmap/Registration.java
+++ app/src/main/java/com/example/eventmap/Registration.java
@@ -1,6 +1,9 @@
 package com.example.eventmap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,15 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        Button registerBtn = findViewById(R.id.registerBtn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Registration.this, Welcome.class);
+                startActivity(intent);
+            }
+        });
     }
 }
