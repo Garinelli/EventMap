63a20e3cc52cbe53695d485de7fe07c1f84e7b24
voskanyan777 <yura.voskanyan.2003@mail.ru>
2025-05-20 17:46:15 +0700

    Added label

--- app/src/main/java/com/example/eventmap/MainView.java
+++ app/src/main/java/com/example/eventmap/MainView.java
@@ -5,10 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.Map;
+import com.yandex.mapkit.map.MapWindow;
 import com.yandex.mapkit.mapview.MapView;
+import com.yandex.runtime.image.ImageProvider;
+
+import com.yandex.mapkit.map.InputListener;
+import com.yandex.mapkit.map.CameraPosition;
 
 public class MainView extends AppCompatActivity {
 
@@ -20,26 +29,40 @@ public class MainView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Ищем MapView по ID
         mapView = findViewById(R.id.mapview);
+
+        // Перемещаем камеру на стартовую точку
+        mapView.getMap().move(
+                new CameraPosition(new Point(55.751574, 37.573856), 10.0f, 0.0f, 0.0f)
+        );
+
+        // Добавляем слушатель нажатий по карте
+        mapView.getMap().addInputListener(new InputListener() {
+            @Override
+            public void onMapTap(@NonNull Map map, @NonNull Point point) {
+                map.getMapObjects().addPlacemark(
+                        point,
+                        ImageProvider.fromResource(MainView.this, R.drawable.baseline_location_on_24)
+                );
+            }
+
+            @Override
+            public void onMapLongTap(@NonNull Map map, @NonNull Point point) {
+                // Можно обработать долгий тап, если нужно
+            }
+        });
 
         Button loginBtn = findViewById(R.id.addPlace);
         ImageView optionsBtn = findViewById(R.id.settingsIcon);
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainView.this, DesignatePlace.class);
-                startActivity(intent);
-            }
+        loginBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainView.this, DesignatePlace.class);
+            startActivity(intent);
         });
 
-        optionsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainView.this, Options.class);
-                startActivity(intent);
-            }
+        optionsBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainView.this, Options.class);
+            startActivity(intent);
         });
     }
 
